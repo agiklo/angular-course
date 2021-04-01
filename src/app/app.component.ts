cdbc93897a7c0c452362ed0cce00d1777eb54e0a
agiklo <54153910+agiklo@users.noreply.github.com>
2021-04-01 22:28:59 +0200

    episode 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-kurs';
+  pi = Math.PI;
+  date = new Date();
+
+  dog = new Dog("Spajki", 12);
+
+  showDog() : string {
+    return 'Mój pies to ' + this.dog.name + ' i ma ' + this.dog.age + ' lat';
+  }
+}
+
+
+class Dog {
+  constructor(public name: string, public age: number) {
+
+  }
 }
