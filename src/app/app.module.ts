b07b2a531be6b04dadd725e1af4d102e3b952b65
agiklo <54153910+agiklo@users.noreply.github.com>
2021-04-01 18:47:31 +0200

    Create inline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { Test3Component } from './test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    Test3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test3/test3.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test3',
+  template: `inline component!`,
+})
+export class Test3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
